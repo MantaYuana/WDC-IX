aa04e47aaa0dc39717740388dd63c7bb57ed9afd
GeryMcGregor <gunamanta.yuana@gmail.com>
2022-08-28 22:33:28 +0800

    Initial Commit

---
+++ api.php
@@ -0,0 +1,4 @@
+<?php
+$PHP_APP_CLIENTID_API_KEY         = '8514593748-ajj7o1acjksbhpd1r5ut9q7k2sji18e9.apps.googleusercontent.com';
+$PHP_APP_CLIENTSECRET_API_KEY     = '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';
+?>
